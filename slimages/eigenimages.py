0e970b8355041da817de777559e504a6927889ee
Arturo Curiel <curieldaz@gmail.com>
2025-07-01 19:19:39 -0600

    Added basic GUI

---
+++ slimages/eigenimages.py
@@ -0,0 +1,11 @@
+# -*- coding:utf-8 -*-
+
+import numpy as np
+import cv2
+from sklearn.decomposition import PCA
+
+def load_images_from_folder(folder_path):
+    pass
+
+def calculate_pca(matrix):
+    pass
---
+++ slimages/main.py
@@ -0,0 +1,89 @@
+# -*- coding:utf-8 -*-
+import sys
+from os.path import expanduser
+
+from PySide6.QtWidgets import (
+    QApplication, QWidget, QPushButton, QLabel,
+    QFileDialog, QVBoxLayout
+)
+
+class MainWindow(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self._input_dir_ready = False
+        self._output_file_ready = False
+
+        self._input_dir_path = None
+        self._output_file_path = None
+
+        self.setWindowTitle("Create eigenimages")
+
+        # Path labels
+        self.input_dir_label = QLabel("Image dir: None selected")
+        self.output_file_label = QLabel("Output file: None selected")
+
+        # Buttons
+        self.select_input_dir_btn = QPushButton("Select input folder")
+        self.select_output_file_btn = QPushButton("Select output image path")
+        self.calculate_btn = QPushButton("Calculate")
+
+        # Deactivate button until paths ready
+        self.calculate_btn.setEnabled(False)
+
+        # Connections
+        self.select_input_dir_btn.clicked.connect(self.select_input_directory)
+        self.select_output_file_btn.clicked.connect(self.select_output_file)
+        self.calculate_btn.clicked.connect(self.calculate)
+
+        # Layout
+        layout = QVBoxLayout()
+        layout.addWidget(self.select_input_dir_btn)
+        layout.addWidget(self.input_dir_label)
+        layout.addWidget(self.select_output_file_btn)
+        layout.addWidget(self.output_file_label)
+        layout.addWidget(self.calculate_btn)
+
+        self.setLayout(layout)
+
+    def select_input_directory(self):
+        dir_path = QFileDialog.getExistingDirectory(self, 
+                                                    "Select input folder", 
+                                                    dir=expanduser("~"))
+        if dir_path:
+            self.input_dir_label.setText(f"Image dir: {dir_path}")
+            self._input_dir_path = dir_path
+            self._input_dir_ready = True
+            self.enable_calculate_btn()
+
+    def select_output_file(self):
+        if self._input_dir_ready:
+            input_dir = self._input_dir_path
+        else:
+            input_dir = expanduser("~")
+
+        file_path, _ = QFileDialog.getSaveFileName(self, 
+                                                   "Select output file",
+                                                   input_dir, 
+                                                   "PNG files(*.png);;All files (*)")
+        if file_path:
+            if not file_path.endswith(".png"):
+                file_path += ".png"
+            self.output_file_label.setText(f"Output file: {file_path}")
+            self._output_file_path = file_path
+            self._output_file_ready = True
+            self.enable_calculate_btn()
+
+    def calculate(self):
+        pass
+
+    def enable_calculate_btn(self):
+        if self._input_dir_ready and self._output_file_ready:
+            self.calculate_btn.setEnabled(True)
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.resize(600, 200)
+    window.show()
+    sys.exit(app.exec())
